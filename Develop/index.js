f45e3d143508745a6d7615d278933742775cf304
Brandon G.M <brandongmerritt@gmail.com>
2021-03-25 14:29:39 -0400

    implemented literals into markdown.js and kept all the questions in the index.js organized based on the markdown structure for semantics

--- Develop/index.js
+++ Develop/index.js
@@ -1,71 +1,127 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+// console.log(generateMarkdown);
 // TODO: Create an array of questions for user input
 const questions = [
-    // username for Github
-    {
-        type: 'input',
-        name: 'username',
-        message: 'Please, enter GitHub username'
-    },
-    // Repo Name from Github
-    {
-        type: 'input',
-        name: 'Repo',
-        message: 'Please, enter the repo name from GitHub.',
-    },
-    // Description of Project
-    {
-        type: 'input',
-        name: 'Project Description',
-        message: 'Enter detailed description of project.',
-    },
-    // How to Install Project
-    {
-        type: 'input',
-        name: 'Install',
-        message: 'Explain how one would implement project to their local drive.',
-    },
-    // Project Use
-    {
-        type: 'input',
-        name: 'Proper Usage',
-        message: 'Instruction and examples of it being used for this section.',
-    },
-    // License
-    {
-        type: 'list', 
-        name: 'license selection',
-        message: 'Select appropiate license for this project.',
-        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
-    },
-    // OpenSource Outlet
-    {
-        type: 'input',
-        name: 'OpenSource',
-        message: 'Demonstrate how other users could contribute to your project',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Portray project tests and show how to test',
-    }
+  // Repo Name from Github
+  {
+    type: "input",
+    name: "title",
+    message: "Please, enter the repo name from GitHub.",
+  },
+
+  // Description of Project
+  {
+    type: "input",
+    name: "projectDescription",
+    message: "Enter detailed description of project.",
+  },
+
+  // How to Install Project
+  {
+    type: "input",
+    name: "Install",
+    message: "Explain how one would implement project to their local drive.",
+  },
+
+  // Project Use
+  {
+    type: "input",
+    name: "properUsage",
+    message: "Instruction and examples of it being used for this section.",
+  },
+
+  // License
+  {
+    type: "list",
+    name: "licenseSelection",
+    message: "Select appropiate license for this project.",
+    choices: [
+      "afl-3.0",
+      "apache-2.0",
+      "artistic-2.0",
+      "bsl-1.0",
+      "bsd-2-clause",
+      "bsd-3-clause",
+      "bsd-3-clause-clear",
+      "cc",
+      "cc0-1.0",
+      "cc-by-4.0",
+      "cc-by-sa-4.0",
+      "wtfpl",
+      "ecl-2.0",
+      "epl-1.0",
+      "epl-2.0",
+      "eupl-1.1",
+      "agpl-3.0",
+      "gpl",
+      "gpl-2.0",
+      "gpl-3.0",
+      "lgpl",
+      "lgpl-2.1",
+      "lgpl-3.0",
+      "isc",
+      "lppl-1.3c",
+      "ms-pl",
+      "mit",
+      "mpl-2.0",
+      "osl-3.0",
+      "postgresql",
+      "ofl-1.1",
+      "ncsa",
+      "unlicense",
+      "zlib",
+    ],
+  },
+
+  // OpenSource Outlet
+  {
+    type: "input",
+    name: "openSource",
+    message: "Demonstrate how other users could contribute to your project",
+  },
+
+  // Testing
+  {
+    type: "input",
+    name: "tests",
+    message: "Portray project tests and show how to test",
+  },
+
+  // username for Github
+  {
+    type: "input",
+    name: "username",
+    message: "Please, enter GitHub username",
+  },
+
+  // Email
+  {
+    type: "input",
+    name: "email",
+    message: "Please, enter email address",
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, error => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Your Readme Markdown manifested.')
-    });
+  fs.writeFile(fileName, data, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Your Readme Markdown manifested.");
+  });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    var templateData = generateMarkdown(data);
+    writeToFile("README.md", templateData);
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `${license}`
+  // badgelink
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +16,26 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Description
+  ${data.projectDescription}
+  ${renderLicenseBadge(data.licenseSelection)}
+  ## Table of Contents
+  ## Installation
+  ${data.Install}
+  ## Usage
+  ${data.properUsage}
+  ## License
+  ${data.licenseSelection}
+  ${renderLicenseBadge(data.licenseSelection)}
+  ## Contributing
+  ${data.openSource}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  If you'd like to connect with the developer about ${data.title} further email them at ${data.email} 
+  gitHub at @${data.username}
 `;
 }
 
 module.exports = generateMarkdown;
+// if picked license then choose corresponding badge mit
